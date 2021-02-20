ee81cc661f4675b366bdece01664299d720c3132
shanfrom90 <shanfrom90@gmail.com>
2021-02-20 16:50:49 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DigitalBookshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your Digital Bookshelf!");
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}, what would you like to do?");
+
+            bool keepBrowsing;
+            switch (keepBrowsing = true)
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
